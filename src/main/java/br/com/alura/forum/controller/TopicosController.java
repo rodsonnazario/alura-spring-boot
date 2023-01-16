054968330d178fc2f14695a1863b711898d7115e
rodsonnazario <rodson.nazario@gmail.com>
2023-01-15 21:29:11 -0300

    ajuste repository no controller

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,22 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
-import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
-		Curso curso = new Curso("Spring", "Programação");
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", curso);
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico)); 
+		return TopicoDto.converter(topicoRepository.findAll());
 	}
 }
