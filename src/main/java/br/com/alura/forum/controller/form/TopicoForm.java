c1687d9399e6b3b9652d54873378c88877e0f336
rodsonnazario <rodson.nazario@gmail.com>
2023-01-19 20:57:57 -0300

    validação com bean validation

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,13 +1,19 @@
 package br.com.alura.forum.controller.form;
 
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
+import jakarta.validation.constraints.NotEmpty;
 
 public class TopicoForm {
 
+	@NotEmpty @Length(min = 5)
 	private String titulo;
+	@NotEmpty @Length(min = 10)
 	private String mensagem;
+	@NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
